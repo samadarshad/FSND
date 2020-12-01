8b29a7be619aa47ecf25823ed32a0a97b3cc4a38
samad <samadarshad@hotmail.co.uk>
2020-12-01 09:23:34 +0000

    fix

--- projects/01_fyyur/starter_code/constants.py
+++ projects/01_fyyur/starter_code/constants.py
@@ -2,4 +2,4 @@ from datetime import timedelta
 
 
 default_availability_slot_duration = timedelta(days=1)
-default_booking_slot_duration = timedelta(days=1)
+default_booking_slot_duration = timedelta(days=0.5)
