7a9e91b87dc6df08ffd85af62e1916f54db3f694
samad <samadarshad@hotmail.co.uk>
2020-12-08 15:48:23 +0000

    Working tests

--- projects/02_trivia_api/starter/backend/README.md
+++ projects/02_trivia_api/starter/backend/README.md
@@ -63,13 +63,21 @@ One note before you delve into your tasks: for each endpoint you are expected to
 
 1. Use Flask-CORS to enable cross-domain requests and set response headers. 
 2. Create an endpoint to handle GET requests for questions, including pagination (every 10 questions). This endpoint should return a list of questions, number of total questions, current category, categories. 
+DONE
 3. Create an endpoint to handle GET requests for all available categories. 
+DONE
 4. Create an endpoint to DELETE question using a question ID. 
-5. Create an endpoint to POST a new question, which will require the question and answer text, category, and difficulty score. 
-6. Create a POST endpoint to get questions based on category. 
+DONE
+5. Create an endpoint to POST a new question, which will require the question and answer text, category, and difficulty score.
+DONE
+6. Create a GET endpoint to get questions based on category. 
+DONE
 7. Create a POST endpoint to get questions based on a search term. It should return any questions for whom the search term is a substring of the question. 
+DONE
 8. Create a POST endpoint to get questions to play the quiz. This endpoint should take category and previous question parameters and return a random questions within the given category, if provided, and that is not one of the previous questions. 
-9. Create error handlers for all expected errors including 400, 404, 422 and 500. 
+DONE
+9. Create error handlers for all expected errors including 400, 404, 422 and 500.
+DONE - but which errors do I trigger where? Let me write tests
 
 REVIEW_COMMENT
 ```
--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}:{}@{}/{}".format('postgres', 'postgres', 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +33,38 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        expected_questions = Question.query.order_by(Question.id).all()
+        expected_questions_formatted = [q.format() for q in expected_questions]
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['questions'], expected_questions_formatted[0:10])
+        self.assertEqual(data['total_questions'], len(expected_questions_formatted))
+
+    def test_404_when_get_questions_beyond_page(self):
+        res = self.client().get('/questions?page=1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], "Resource not found")
+
+    def test_add_question(self):
+        delete_question = Question.query.filter(Question.question=='q1').all()
+        [q.delete() for q in delete_question]
+        delete_question = Question.query.filter(Question.question=='q1').all()
+        self.assertFalse(delete_question)
+        
+        res = self.client().post('/questions', json={'question': 'q1', 'answer': 'a1', 'difficulty': 5, 'category': 1})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        question = Question.query.filter(Question.question=='q1').first()
+        self.assertTrue(question)
+    # when wanting to test DELETE endpoint, do this: create a new question via backend, then delete using frontend endpoint - https://knowledge.udacity.com/questions/312483
 
 
 # Make the tests conveniently executable
