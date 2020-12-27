f70b25bb1ba5783cfed246579451c726f50c0f42
samad <samadarshad@hotmail.co.uk>
2020-12-27 20:29:06 +0000

    view tests

--- projects/capstone/vaccines_monitor/tests.py
+++ projects/capstone/vaccines_monitor/tests.py
@@ -9,19 +9,11 @@ test_api = Blueprint('test_api', __name__)
 @test_api.route('', methods=['GET'])
 @requires_auth('read:all_tests')
 def getTests(jwt):
-        
-    #TODO
+    page = request.args.get('page', 1, type=int)
+    items_per_page = request.args.get('items_per_page', 10, type=int)
+    tests = Test.query.order_by(Test.id).paginate(page, items_per_page, error_out=False)
+    current_tests = [t.format() for t in tests.items]
+    total_number = len(Test.query.all())
 
-    return jsonify({
-        'success': True
-        })
+    return jsonify({'tests': current_tests, 'total_number': total_number})       
 
-@test_api.route('/<id>', methods=['GET'])
-@requires_auth('read:all_tests')
-def getTest(jwt, id):
-        
-    #TODO
-
-    return jsonify({
-        'success': True
-        })
