e511466b9049b534446ff1d1e1ed3007791c0338
samad <samadarshad@hotmail.co.uk>
2020-12-28 13:13:25 +0000

    reverting tests removal

--- projects/capstone/vaccines_monitor/tests.py
+++ projects/capstone/vaccines_monitor/tests.py
@@ -12,3 +12,10 @@ def getTests(jwt):
     page = request.args.get('page', 1, type=int)
     items_per_page = request.args.get('items_per_page', 10, type=int)
     return jsonify(getPaginatedTable(Test, page, items_per_page))
+
+
+@test_api.route('/<id>', methods=['GET'])
+@requires_auth('read:all_tests')
+def getTest(jwt, id):
+    test = getInstanceOrAbort(Test, id)
+    return jsonify(test.format())
